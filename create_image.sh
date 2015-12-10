a0675512e12f5704d814b350cd693fc520e08b40
Manoj Mallela <manojmallela@users.noreply.github.com>
2015-12-10 11:21:54 -0500

    Update maven version to 3.3.3

--- create_image.sh
+++ create_image.sh
@@ -48,14 +48,14 @@ done
 
 # Install Maven (for Hadoop)
 cd /tmp
-wget "http://archive.apache.org/dist/maven/maven-3/3.2.3/binaries/apache-maven-3.2.3-bin.tar.gz"
-tar xvzf apache-maven-3.2.3-bin.tar.gz
-mv apache-maven-3.2.3 /opt/
+wget "http://archive.apache.org/dist/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz"
+tar xvzf apache-maven-3.3.3-bin.tar.gz
+mv apache-maven-3.3.3 /opt/
 
 # Edit bash profile
 echo "export PS1=\"\\u@\\h \\W]\\$ \"" >> ~/.bash_profile
 echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0" >> ~/.bash_profile
-echo "export M2_HOME=/opt/apache-maven-3.2.3" >> ~/.bash_profile
+echo "export M2_HOME=/opt/apache-maven-3.3.3" >> ~/.bash_profile
 echo "export PATH=\$PATH:\$M2_HOME/bin" >> ~/.bash_profile
 
 source ~/.bash_profile
